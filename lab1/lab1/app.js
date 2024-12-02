c68615a396d2cd3b699d1d11f8a51ba1e044890e
hpiotrowski <piotrowski.hubert04@gmail.com>
2024-12-02 14:21:05 +0100

    pokev1

--- lab1/lab1/app.js
+++ lab1/lab1/app.js
@@ -1,5 +1,5 @@
-
 const pokemonURL = "https://pokeapi.co/api/v2/pokemon";
+
 async function pobierzDane(pokemonName) {
     const url = `${pokemonURL}/${pokemonName.toLowerCase()}`;
     try {
@@ -12,31 +12,68 @@ async function pobierzDane(pokemonName) {
     } catch (error) {
         console.error(error.message);
         displayErrorMessage(error.message);
-    } finally {
-  
     }
 }
+
+
 function displayPokemona(pokemon) {
     const detailsElement = document.getElementById("pokemonDetails");
     detailsElement.innerHTML = `
         <h3>${pokemon.name.toUpperCase()}</h3>
         <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}">
-        <p>Waga: ${pokemon.weight}</p>
-        <p>Wzrost: ${pokemon.height}</p>
-        <p>Typy: ${pokemon.types.map(typy => typy.type.name).join(", ")}</p>
+        <p><strong>Waga:</strong> ${pokemon.weight / 10} kg</p>
+        <p><strong>Wzrost:</strong> ${pokemon.height / 10} m</p>
+        <p><strong>Typy:</strong> ${pokemon.types.map(type => type.type.name).join(", ")}</p>
     `;
+    const backButton = document.createElement("button");
+    backButton.textContent = "Wróć do listy";
+    backButton.addEventListener("click", () => listaPokemonow());
+    detailsElement.appendChild(backButton);
 }
+
+
 
 function displayErrorMessage(message) {
     const detailsElement = document.getElementById("pokemonDetails");
     detailsElement.innerHTML = `<p style="color: red;">${message}</p>`;
 }
 
+
 document.getElementById("searchButton").addEventListener("click", () => {
     const searchInput = document.getElementById("searchInput").value.trim();
     if (searchInput) {
         pobierzDane(searchInput);
     } else {
-        displayErrorMessage("Wpisz nazwe");
+        displayErrorMessage("Wpisz nazwę Pokemona.");
     }
 });
+
+
+async function listaPokemonow(limit = 20) {
+    try {
+        const response = await fetch(`${pokemonURL}?limit=${limit}`);
+        if (!response.ok) {
+            throw new Error("Nie udało się pobrać listy Pokemonów.");
+        }
+        const data = await response.json();
+        displayListaPokemonow(data.results);
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+
+
+function displayListaPokemonow(pokemons) {
+    const detailsElement = document.getElementById("pokemonDetails");
+    detailsElement.innerHTML = "<h3>Lista Pokemonów:</h3>";
+    pokemons.forEach((pokemon, index) => {
+        const button = document.createElement("button");
+        button.textContent = `${index + 1}. ${pokemon.name}`;
+        button.addEventListener("click", () => pobierzDane(pokemon.name));
+        detailsElement.appendChild(button);
+    });
+}
+
+
+
+listaPokemonow();
